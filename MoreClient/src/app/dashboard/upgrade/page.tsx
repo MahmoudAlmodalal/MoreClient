06b6d5512efe8d65b915a2281b70237129283150
Mahmoud Hisham Almodalal <ghg17273@gmail.com>
2026-06-07 05:28:05 +0000

    v

--- MoreClient/src/app/dashboard/upgrade/page.tsx
+++ MoreClient/src/app/dashboard/upgrade/page.tsx
@@ -1,7 +1,15 @@
-import { UpgradeClient } from "@/components/dashboard/upgrade-client";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const UpgradeClient = dynamic(
+  () => import("@/components/dashboard/upgrade-client").then((mod) => mod.UpgradeClient),
+  { ssr: false }
+);
 
 export default function UpgradePage() {
   return <UpgradeClient />;
 }
+
 
 
